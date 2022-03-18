a3996ff40c855bef366f8e1a8a4aa690d1d59f99
Jrevoredo42 <revoredojf@gmail.com>
2022-03-18 09:48:54 -0300

    foi feito conexao da lista de repositorios com o componente de item da lista para reproduçao desses repositorios pegos atraves da API, alem dos comentarios que descrevem a execuçao do codigo

--- src/Components/RepositoryItem.jsx
+++ src/Components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem (props){
     return(
         <li>
-            <strong><p>{props.repository?.name ?? 'default'}</p></strong>
+            <strong><p>{props.repository?.name}</p></strong>
             <br/>
 
-            <a href={props.repository?.url}>
+            <a href={props.repository?.html_url}>
                 Acessar repositorio
             </a>
         </li>
--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -3,33 +3,28 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
 
-
-
-const repository = {
-    name: 'Formulario de Github',
-    description: 'repository example',
-    url: 'https://github.com/Jrevoredo42'
-}
-
 export function RepositoryList (){
 
+    //State de repositorio pra utilizar na chamada da API, tendo um array vazio como valor inicial
     const [repositories, setRepositories] = useState([]);
 
+    /*Hook useEffect, utilizado para fazer a chamada da api,
+    converter a resposta num json, e pegar seus dados para o state de repositorio (array de repositorios)*/
     useEffect(() => {
        fetch('https://api.github.com/users/Jrevoredo42/repos')
        .then(response => response.json())
-       .then(data => console.log(data))
+       .then(data => setRepositories(data))
     }, [])
 
-
+    /*Sessão com lista de repositorios, que utiliza a função Map pra percorrer os valores de repositorio
+    e retorna-los dentro do component RepositoryItem  */
     return(
         <section className='repository-List'>
             <h1>Lista de Repositorios</h1>
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem />
-                <RepositoryItem />
+               {repositories.map(repository => {
+                   return <RepositoryItem repository={repository} />
+               })}
             </ul>
         </section>
     )
