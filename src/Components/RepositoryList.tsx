bb0b5ff2cebed8e271278840a3e6ec97e1a56299
Jrevoredo42 <revoredojf@gmail.com>
2022-03-19 12:21:17 -0300

    conversao dos componentes para typescript

--- src/Components/RepositoryList.tsx
+++ src/Components/RepositoryList.tsx
@@ -2,14 +2,26 @@ import { useState, useEffect  } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
+/* Interface necessaria para declarar tipagem no state, permitindo tanto a realização da API,
+ como também a declaração dos props do component RepositoryList*/
+
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 
 export function RepositoryList (){
 
-    //State de repositorio pra utilizar na chamada da API, tendo um array vazio como valor inicial
-    const [repositories, setRepositories] = useState([]);
+    /*State de repositorio pra utilizar na chamada da API, tendo um array com a tipagem da interface Repository 
+    vazio  como valor inicial*/
 
-    /*Hook useEffect, utilizado para fazer a chamada da api,
-    converter a resposta num json, e pegar seus dados para o state de repositorio (array de repositorios)*/
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    /*Hook useEffect, utilizado para fazer a chamada da API,
+    converter a resposta num json e pegar seus dados para o state de repositorio (array de repositorios)*/
+
     useEffect(() => {
        fetch('https://api.github.com/users/Jrevoredo42/repos')
        .then(response => response.json())
@@ -18,6 +30,7 @@ export function RepositoryList (){
 
     /*Sessão com lista de repositorios, que utiliza a função Map pra percorrer os valores de repositorio
     e retorna-los dentro do component RepositoryItem  */
+
     return(
         <section className='repository-List'>
             <h1>Lista de Repositorios</h1>
